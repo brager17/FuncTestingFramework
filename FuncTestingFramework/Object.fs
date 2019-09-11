1391e9db9e02faa811069a1429acfaee4cbf252a
Evgeny Terekhin <eter55@yandex.ru>
2019-09-11 08:11:42 +0300

    refactoring f# version

--- FuncTestingFramework/Object.fs
+++ FuncTestingFramework/Object.fs
@@ -48,15 +48,20 @@ type Object<'a>(storage: Storage<'a>) =
     member __.For(path: Expression<Func<'a, IEnumerable<'c>>>) =
         SequenceClassT<'a, 'c>([], path)
 
+type PathObject<'a, 'd>(store: Storage<'a>, path: Path<'a, 'd>) =
+    inherit Object<'a>(store)
+    abstract UseValue: 'd -> Object<'a>
+    abstract Ignore: unit -> Object<'a>
+    default  __.UseValue(value: 'd) =
+        Object<'a>((ObjectExpressions.useValue path value) :: store)
+    default __.Ignore() =
+        Object<'a>((ObjectExpressions.ignore path) :: store)
+        
 type NestedClassConfiguration<'a> = Func<Object<'a>, Object<'a>>
 type ClassT<'a, 'd>(store: Storage<'a>, path: Path<'a, 'd>) =
-    inherit Object<'a>(store)
-    member __.UseValue((value: 'd)) =
-        ClassT((ObjectExpressions.useValue path value) :: store, path)
-
-    member __.Ignore() =
-        ClassT((ObjectExpressions.ignore path) :: store, path)
-
+    inherit PathObject<'a, 'd>(store, path)
+    override  __.UseValue(value: 'd) = base.UseValue(value)
+    override __.Ignore() = base.Ignore()
     member __.ForNested(deepPath: NestedClassConfiguration<'d>): Object<'a> =
         let objD = deepPath.Invoke(Object<'d>([]))
         let func = ObjectExpressions.forNested path objD.Storage
@@ -64,17 +69,14 @@ type ClassT<'a, 'd>(store: Storage<'a>, path: Path<'a, 'd>) =
 
 type SequenceNestedClassConfiguration<'a> = Func<Object<'a>, Object<'a>>
 type SequenceClassT<'a, 'd>(store: Storage<'a>, path: Path<'a, IEnumerable<'d>>) =
-    inherit Object<'a>(store)
-    member __.UseValue(value: IEnumerable<'d>) =
-        SequenceClassT(((ObjectExpressions.useValue path value) :: store), path)
-
-    member __.Ignore() =
-        SequenceClassT(((ObjectExpressions.ignore path) :: store), path)
+    inherit PathObject<'a, IEnumerable<'d>>(store, path)
+    override  __.UseValue(value: IEnumerable<'d>) = base.UseValue(value)
+    override __.Ignore() = base.Ignore()
 
     member __.ForItem(storage: SequenceNestedClassConfiguration<'d>) =
         let mutableFunc =
             let nullElement = Object<'d>([])
-            let storage = storage.Invoke(nullElement).Storage |> Seq.map(fun x -> x.Compile()) |> Seq.toList
+            let storage = storage.Invoke(nullElement).Storage |> Seq.map (fun x -> x.Compile()) |> Seq.toList
             <@
             fun x ->
                let res = path.Compile().Invoke(x)
@@ -87,12 +89,9 @@ type SequenceClassT<'a, 'd>(store: Storage<'a>, path: Path<'a, IEnumerable<'d>>)
         SequenceClassT(mutableFunc :: store, path)
 
 type DecimalT<'a>(store: Storage<'a>, path: Path<'a, decimal>) =
-     inherit Object<'a>(store)
-     member __.UseValue(value: decimal) =
-        DecimalT(((ObjectExpressions.useValue path value) :: store), path)
-
-     member __.Ignore() =
-        DecimalT(((ObjectExpressions.ignore path) :: store), path)
+    inherit PathObject<'a, decimal>(store, path)
+    override  __.UseValue(value: decimal) = base.UseValue(value)
+    override __.Ignore() = base.Ignore()
 
      member __.Min(min: decimal) =
         DecimalT((DecimalExpressions.Interval path min Decimal.MaxValue) :: store, path)
@@ -107,13 +106,9 @@ type DecimalT<'a>(store: Storage<'a>, path: Path<'a, decimal>) =
 type DecimalItemConfiguration = Func<DecimalT<Value<decimal>>, DecimalT<Value<decimal>>>
 
 type SequenceDecimal<'a>(store: Storage<'a>, path: Path<'a, IEnumerable<decimal>>) =
-     inherit Object<'a>(store)
-
-     member __.UseValue(value: IEnumerable<decimal>) =
-        SequenceDecimal(((ObjectExpressions.useValue path value) :: store), path)
-
-     member __.Ignore() =
-        SequenceDecimal(((ObjectExpressions.ignore path) :: store), path)
+    inherit PathObject<'a, IEnumerable<decimal>>(store, path)
+    override  __.UseValue(value: IEnumerable<decimal>) = base.UseValue(value)
+    override __.Ignore() = base.Ignore()
 
      member __.ForItem(storage: DecimalItemConfiguration) =
         let mutableFunc =
@@ -123,37 +118,29 @@ type SequenceDecimal<'a>(store: Storage<'a>, path: Path<'a, IEnumerable<decimal>
         SequenceDecimal(mutableFunc :: store, path)
 
 
-type StringT<'a>(store: Storage<'a>, expr: Path<'a, string>) =
-     inherit Object<'a>(store)
-
-     member __.UseValue(value: string) =
-        StringT((ObjectExpressions.useValue expr value) :: store, expr)
-
-     member __.Ignore() =
-        StringT((ObjectExpressions.ignore expr) :: store, expr)
+type StringT<'a>(store: Storage<'a>, path: Path<'a, string>) =
+    inherit PathObject<'a, string>(store, path)
+    override  __.UseValue(value: string) = base.UseValue(value)
+    override __.Ignore() = base.Ignore()
 
      member __.Length(value: int) =
-        StringT((StringExpressions.intervalLengthStringMethod expr value value) :: store, expr)
+        StringT((StringExpressions.intervalLengthStringMethod path value value) :: store, path)
 
      member __.MinLength(value: int) =
         if value > 10000 || value < 0 then failwith "value must be less 10000"
-        StringT((StringExpressions.intervalLengthStringMethod expr value 1000) :: store, expr)
+        StringT((StringExpressions.intervalLengthStringMethod path value 1000) :: store, path)
 
      member __.MaxLength(value: int) =
         if value > 10000 || value < 0 then failwith "value must be less 10000"
-        StringT((StringExpressions.intervalLengthStringMethod expr 0 value) :: store, expr)
+        StringT((StringExpressions.intervalLengthStringMethod path 0 value) :: store, path)
 
 
 type StringItemConfiguration = Func<StringT<Value<string>>, StringT<Value<string>>>
 
 type SequenceString<'a>(store: Storage<'a>, path: Path<'a, IEnumerable<string>>) =
-      inherit Object<'a>(store)
-
-      member __.UseValue(value: IEnumerable<string>) =
-        SequenceString(((ObjectExpressions.useValue path value) :: store), path)
-
-      member __.Ignore() =
-        SequenceString(((ObjectExpressions.ignore path) :: store), path)
+    inherit PathObject<'a, IEnumerable<string>>(store, path)
+    override  __.UseValue(value: IEnumerable<string>) = base.UseValue(value)
+    override __.Ignore() = base.Ignore()
 
       member __.ForItem(storage: StringItemConfiguration) =
         let mutableFunc =
@@ -164,7 +151,9 @@ type SequenceString<'a>(store: Storage<'a>, path: Path<'a, IEnumerable<string>>)
 
 
 type IntT<'a>(store: Storage<'a>, path: Path<'a, int>) =
-    inherit Object<'a>(store)
+    inherit PathObject<'a, int>(store, path)
+    override  __.UseValue(value: int) = base.UseValue(value)
+    override __.Ignore() = base.Ignore()
 
     member __.Between((l: int), (r: int)): IntT<'a> =
         if l >= r then failwith "left number must be less than rigth number"
@@ -182,91 +171,68 @@ type IntT<'a>(store: Storage<'a>, path: Path<'a, int>) =
     member __.Positive(): IntT<'a> =
         IntT((IntExpressions.Interval path 0 Int32.MaxValue :: store), path)
 
-    member __.UseValue(v: int): IntT<'a> =
-         IntT(ObjectExpressions.useValue path v :: store, path)
-
-    member __.Ignore(): IntT<'a> =
-         IntT(ObjectExpressions.ignore path :: store, path)
-
 
 type SequenceIntConfiguration = Func<IntT<Value<int>>, IntT<Value<int>>>
 
 type SequenceInt<'a>(store: Storage<'a>, path: Path<'a, IEnumerable<int>>) =
-      inherit Object<'a>(store)
+    inherit PathObject<'a, IEnumerable<int>>(store, path)
+    override  __.UseValue(value: IEnumerable<int>) = base.UseValue(value)
+    override __.Ignore() = base.Ignore()
 
-      member __.UseValue(value: IEnumerable<int>) =
-        SequenceInt(((ObjectExpressions.useValue path value) :: store), path)
-
-      member __.Ignore() =
-        SequenceInt(((ObjectExpressions.ignore path) :: store), path)
-
-      member __.ForItem(storage: SequenceIntConfiguration) =
+    member __.ForItem(storage: SequenceIntConfiguration) =
         let mutableFunc =
-            let nullElement = IntT([], Value<int>.GetLambda())
-            let storage = storage.Invoke(nullElement).Storage
-            ObjectExpressions.forItem path storage
+          let nullElement = IntT([], Value<int>.GetLambda())
+          let storage = storage.Invoke(nullElement).Storage
+          ObjectExpressions.forItem path storage
         SequenceInt(mutableFunc :: store, path)
 
 
-type DateTimeT<'a>(store: Storage<'a>, expr: Path<'a, DateTime>) =
-    inherit Object<'a>(store)
-     member __.UseValue(value: DateTime) =
-        DateTimeT(((ObjectExpressions.useValue expr value) :: store), expr)
-
-    member __.Ignore() =
-        DateTimeT((ObjectExpressions.ignore expr) :: store, expr)
+type DateTimeT<'a>(store: Storage<'a>, path: Path<'a, DateTime>) =
+    inherit PathObject<'a, DateTime>(store, path)
+    override  __.UseValue(value: DateTime) = base.UseValue(value)
+    override __.Ignore() = base.Ignore()
 
     member __.Min(min: DateTime) =
-        DateTimeT((DateTimeExpressions.Interval expr min DateTime.MaxValue) :: store, expr)
+        DateTimeT((DateTimeExpressions.Interval path min DateTime.MaxValue) :: store, path)
 
     member __.Max(max: DateTime) =
-        DateTimeT((DateTimeExpressions.Interval expr DateTime.MinValue max) :: store, expr)
+        DateTimeT((DateTimeExpressions.Interval path DateTime.MinValue max) :: store, path)
 
     member __.Interval (min: DateTime) (max: DateTime) =
-        DateTimeT((DateTimeExpressions.Interval expr min max) :: store, expr)
+        DateTimeT((DateTimeExpressions.Interval path min max) :: store, path)
 
 
 type SequenceDateTimeConfiguration = Func<DateTimeT<Value<DateTime>>, DateTimeT<Value<DateTime>>>
 
 type DateTimeSequence<'a>(store: Storage<'a>, path: Path<'a, IEnumerable<DateTime>>) =
-         inherit Object<'a>(store)
+     inherit PathObject<'a, IEnumerable<DateTime>>(store, path)
+     override  __.UseValue(value: IEnumerable<DateTime>) = base.UseValue(value)
+     override __.Ignore() = base.Ignore()
 
-         member __.UseValue(value: IEnumerable<DateTime>) =
-             DateTimeSequence(((ObjectExpressions.useValue path value) :: store), path)
-
-         member __.Ignore() =
-            DateTimeSequence(((ObjectExpressions.ignore path) :: store), path)
-
-         member __.ForItem(storage: SequenceDateTimeConfiguration) =
-           let mutableFunc =
-               let nullElement = DateTimeT([], Value<DateTime>.GetLambda())
-               let storage = storage.Invoke(nullElement).Storage
-               ObjectExpressions.forItem path storage
-           DateTimeSequence(mutableFunc :: store, path)
+     member __.ForItem(storage: SequenceDateTimeConfiguration) =
+       let mutableFunc =
+           let nullElement = DateTimeT([], Value<DateTime>.GetLambda())
+           let storage = storage.Invoke(nullElement).Storage
+           ObjectExpressions.forItem path storage
+       DateTimeSequence(mutableFunc :: store, path)
 
 type BooleanT<'a>(store: Storage<'a>, path: Path<'a, bool>) =
-    inherit Object<'a>(store)
-    member __.UseValue(value: bool) =
-             BooleanT(((ObjectExpressions.useValue path value) :: store), path)
-
-    member __.Ignore() =
-            BooleanT(((ObjectExpressions.ignore path) :: store), path)
+    inherit PathObject<'a, bool>(store, path)
+    override __.UseValue(value: bool) = __.UseValue(value)
+    override __.Ignore() = __.Ignore()
 
 
 type BooleanSequenceConfiguration = Func<BooleanT<Value<bool>>, BooleanT<Value<bool>>>
 
 type BooleanSequence<'a>(store: Storage<'a>, path: Path<'a, IEnumerable<bool>>) =
-         inherit Object<'a>(store)
-         member __.UseValue(value: IEnumerable<bool>) =
-             BooleanSequence(((ObjectExpressions.useValue path value) :: store), path)
+     inherit PathObject<'a, IEnumerable<bool>>(store, path)
+     override  __.UseValue(value: IEnumerable<bool>) = base.UseValue(value)
+     override __.Ignore() = base.Ignore()
 
-         member __.Ignore() =
-            BooleanSequence(((ObjectExpressions.ignore path) :: store), path)
-
-         member __.ForItem(storage: BooleanSequenceConfiguration) =
-           let mutableFunc =
-               let nullElement = BooleanT([], Value<bool>.GetLambda())
-               let storage = storage.Invoke(nullElement).Storage
-               ObjectExpressions.forItem path storage
-           BooleanSequence(mutableFunc :: store, path)
+     member __.ForItem(storage: BooleanSequenceConfiguration) =
+       let mutableFunc =
+           let nullElement = BooleanT([], Value<bool>.GetLambda())
+           let storage = storage.Invoke(nullElement).Storage
+           ObjectExpressions.forItem path storage
+       BooleanSequence(mutableFunc :: store, path)
 
